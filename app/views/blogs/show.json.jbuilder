be69a3b15662437e2cee811dfa912b1eede9355d
Niu Yameng <mengmeng0927@gmail.com>
2014-05-15 00:10:31 +1000

    add blog mvc

---
+++ app/views/blogs/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @blog, :id, :title, :content, :picture, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 BlogDemo::Application.routes.draw do
+  resources :blogs
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
